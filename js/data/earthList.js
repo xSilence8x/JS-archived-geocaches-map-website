9bac50537cbc91ef2925d285fda7588b1fab3825
Marketa Dalecka <50072173+xSilence8x@users.noreply.github.com>
2023-08-24 14:30:53 +0200

    Create earthList.js

---
+++ js/data/earthList.js
@@ -0,0 +1,14 @@
+var earthList = [
+{nazev: "	Brnenska prehrada, jak ji neznate / Brno Dam	" , GC: "	GC2BCX5	" , coords: [	49.2299500, 16.5178000	]},
+{nazev: "	Permokarbonem / Permocarbon Boskovicka brazda	" , GC: "	GC2BH3Z	" , coords: [	49.1281333, 16.3311667	]},
+{nazev: "	Hamerske vrasy / Hamry Rock Folds	" , GC: "	GC16M06	" , coords: [	48.8941667, 15.8216667	]},
+{nazev: "	Ruzovy vrch/The Pink Hill	" , GC: "	GC3MJMD	" , coords: [	48.8451500, 16.6400333	]},
+{nazev: "	Garnet Creek	" , GC: "	GC2CF6Q	" , coords: [	48.9095833, 15.8230333	]},
+{nazev: "	PR Brencak(slepence)	" , GC: "	GC6VM32	" , coords: [	49.2759833, 16.4579333	]},
+{nazev: "	Kamen s krizem	" , GC: "	GC3NME5	" , coords: [	49.5602000, 16.4259833	]},
+
+
+
+
+
+];

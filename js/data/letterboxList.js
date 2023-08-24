7dcfe778714be2bfba83b1dede1eb5598a79ca59
Marketa Dalecka <50072173+xSilence8x@users.noreply.github.com>
2023-08-24 14:31:31 +0200

    Add files via upload

---
+++ js/data/letterboxList.js
@@ -0,0 +1,27 @@
+﻿var letterboxList = [
+{nazev: "	Ⓐ Anatidaefobie	" , GC: "	GC5EJZB	" , coords: [	49.1988167, 16.5354833	]},
+{nazev: "	♨ Teplej T5 letterbox ✉	" , GC: "	GC4W99R	" , coords: [	49.220933, 16.493967	]},
+{nazev: "	Cernobyl 2017	" , GC: "	GC7GGJF	" , coords: [	49.3519333, 16.4092167	]},
+{nazev: "	Colossus	" , GC: "	GC112B1	" , coords: [	49.182683, 16.606983	]},
+{nazev: "	Dopisni	" , GC: "	GC2ZWM1	" , coords: [	49.237067, 16.603833	]},
+{nazev: "	GeoTube Lockpicking	" , GC: "	GC7V4HN	" , coords: [	48.958333, 17.290833	]},
+{nazev: "	Hastrman na Dvorskem rybniku v.2	" , GC: "	GC2R61M	" , coords: [	49.01455, 16.756067	]},
+{nazev: "	Lehkej box	" , GC: "	GC8ZQPR	" , coords: [	49.3456000, 16.4202833	]},
+{nazev: "	Lesni cesty 	" , GC: "	GC6CEV5	" , coords: [	49.279367, 16.743567	]},
+{nazev: "	Olsi - delnicka kolonie	" , GC: "	GC6JPCM	" , coords: [	49.223217, 16.6404	]},
+{nazev: "	Po stopách Járy Cimrmana	" , GC: "	GC8AFTJ	" , coords: [	49.4043833, 16.4130167	]},
+{nazev: "	Poklad loupeznika Babinskeho	" , GC: "	GC7CDET	" , coords: [	49.2041000, 16.6681167	]},
+{nazev: "	Quest for the Chamber of Secrets	" , GC: "	GC2X8ZX	" , coords: [	49.1442, 16.606717	]},
+{nazev: "	Skola bankovniho lupice	" , GC: "	GC3JF6T	" , coords: [	49.348883, 16.421817	]},
+{nazev: "	Strelnice	" , GC: "	GC7Q8WC	" , coords: [	49.052183, 16.315917	]},
+{nazev: "	Vyskov - ZOO	" , GC: "	GC1HH1E	" , coords: [	49.2625, 16.972667	]},
+{nazev: "	Zahada hlavolamu	" , GC: "	GC35J62	" , coords: [	49.194867, 16.555833	]},
+{nazev: "	Zakladni kacersky vycvik	" , GC: "	GC4XYJF	" , coords: [	49.333, 16.508517	]},
+
+
+
+
+
+
+
+];
---
+++ js/data/wherigoList.js
@@ -0,0 +1,22 @@
+﻿var wherigoList = [
+{nazev: "	Certova rokle	" , GC: "	GC1AT9C	" , coords: [	49.227967, 16.6225	]},
+{nazev: "	Krumlovska reverse cache	" , GC: "	GC7H9HE	" , coords: [	49.044917, 16.304683	]},
+{nazev: "	Krysi Stola na Spilberku	" , GC: "	GC5Z73B	" , coords: [	49.193917, 16.59995	]},
+{nazev: "	Le Tour de Brno - Downhill	" , GC: "	GC20VHW	" , coords: [	49.261517, 16.4981	]},
+{nazev: "	Loucky klaster	" , GC: "	GC2KJ1E	" , coords: [	48.842767, 16.060517	]},
+{nazev: "	Loupezne prepadeni	" , GC: "	GC5AM0X	" , coords: [	49.166, 16.7261	]},
+{nazev: "	Projizdka II	" , GC: "	GC584VR	" , coords: [	49.481167, 16.689167	]},
+{nazev: "	Projizdka	" , GC: "	GC4TC9D	" , coords: [	49.481167, 16.689167	]},
+{nazev: "	Zapeklity pripad	" , GC: "	GC2D0K8	" , coords: [	49.2048, 16.594467	]},
+{nazev: "	Sazinek	" , GC: "	GC22VKW	" , coords: [	49.2187833, 16.5542667	]},
+{nazev: "	Ostra Horka	" , GC: "	GC4601T	" , coords: [	49.2454667, 16.6244167	]},
+{nazev: "	Utek z rudeho mesta	" , GC: "	GC28G82	" , coords: [	49.3707500, 16.6402000	]},
+{nazev: "	(Per)R(e)verzni wherigo v Tigalu	" , GC: "	GC811G6	" , coords: [	49.3522000, 16.4295500	]},
+{nazev: "	Po stopach vcel	" , GC: "	GC6BMEH	" , coords: [	49.4065667, 16.7012167	]},
+{nazev: "	Bzenecky kastelan	" , GC: "	GC6JJT5	" , coords: [	48.9717167, 17.2666500	]},
+
+
+
+
+
+];
